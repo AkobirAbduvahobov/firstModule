acddebd3628327036faa67ee20448cbe7b9b2b80
Akobir <akobirabduvahobov@gmail.com>
2024-11-23 09:43:19 +0500

    9 dars

---
+++ 9-dars/9-dars.Api/Models/Car.cs
@@ -0,0 +1,12 @@
+﻿namespace _9_dars.Api.Models;
+
+public class Car
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Model { get; set; }
+
+    public double Price { get; set; }
+}
---
+++ 9-dars/9-dars.Api/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _9_dars.Api;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+
+
