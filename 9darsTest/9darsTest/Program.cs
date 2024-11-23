9e8b48aba6c9e4e795be4e3595c89205028d1f53
Akobir <akobirabduvahobov@gmail.com>
2024-11-23 09:55:01 +0500

    GitIgnore Readme added

---
+++ 9darsTest/9darsTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _9darsTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
